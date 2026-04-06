d995a45b6b3be8749e92a4fc2adcdb77668459a0
Rohit Patil <rp7938008@gmail.com>
2026-04-06 23:32:29 +0530

    fix cors + remove credentials

--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,28 @@
 require("dotenv").config();
 
 const path = require("path");
+const cors = require("cors"); 
+
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
 
 connectToDB();
 
-//  Serve frontend
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+// Serve frontend
 app.use(require("express").static(path.join(__dirname, "dist")));
 
-// Fallback route
+// Fallback
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-// Start server
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -4,7 +4,7 @@ import axios from "axios"
 const api = axios.create({
     // baseURL: "http://localhost:3000",
     baseURL:"",
-    withCredentials: true
+    // withCredentials: true
 })
 
 export async function register({ username, email, password }) {
