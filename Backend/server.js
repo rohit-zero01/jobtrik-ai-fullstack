a32eba6b6c9011bb332058ef556d9ce4983eee1d
Rohit Patil <rp7938008@gmail.com>
2026-04-06 22:32:02 +0530

    setup single deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,20 @@
-require("dotenv").config()
-const app = require("./src/app")
-const connectToDB = require("./src/config/database")
+require("dotenv").config();
 
-connectToDB()
+const path = require("path");
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
 
+connectToDB();
 
+//  Serve frontend
+app.use(require("express").static(path.join(__dirname, "dist")));
+
+// Fallback route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+// Start server
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+  console.log("Server is running on port 3000");
+});
