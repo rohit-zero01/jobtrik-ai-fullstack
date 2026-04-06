944cb7cba7919139f2afa24e7bd8a44c45c0ca63
Rohit Patil <rp7938008@gmail.com>
2026-04-06 23:23:03 +0530

    fix api routing mismatch

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -3,14 +3,14 @@ import axios from "axios"
 
 const api = axios.create({
     // baseURL: "http://localhost:3000",
-    baseURL:"/api",
+    baseURL:"",
     withCredentials: true
 })
 
 export async function register({ username, email, password }) {
 
     try {
-        const response = await api.post('/auth/register', {
+        const response = await api.post('/api/auth/register', {
             username, email, password
         })
 
@@ -28,7 +28,7 @@ export async function login({ email, password }) {
 
     try {
 
-        const response = await api.post("/auth/login", {
+        const response = await api.post("/api/auth/login", {
             email, password
         })
 
@@ -49,7 +49,7 @@ return data;
 export async function logout() {
     try {
 
-        const response = await api.get("/auth/logout")
+        const response = await api.get("/api/auth/logout")
 
         return response.data
 
@@ -62,10 +62,11 @@ export async function getMe() {
 
     try {
 
+        // const response = await api.get("/api/auth/get-me")
 
         const token = localStorage.getItem("token");
 
-const response = await api.get("/auth/get-me", {
+const response = await api.get("/api/auth/get-me", {
     headers: {
         Authorization: `Bearer ${token}`
     }
